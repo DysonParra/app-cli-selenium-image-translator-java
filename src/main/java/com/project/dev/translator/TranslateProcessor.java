18c901c49fa6017586fa602b5d74a145c31915e2
Dyson <dysontilano@gmail.com>
2022-09-30 19:24:18 -0500

    Delegate take node screenshot to a new class

--- src/main/java/com/project/dev/translator/TranslateProcessor.java
+++ src/main/java/com/project/dev/translator/TranslateProcessor.java
@@ -12,26 +12,18 @@
 package com.project.dev.translator;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.Files;
 import com.project.dev.flag.processor.Flag;
 import com.project.dev.flag.processor.FlagMap;
 import com.project.dev.generic.processor.FileProcessor;
 import com.project.dev.generic.processor.SeleniumProcessor;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import javax.imageio.ImageIO;
 import lombok.Data;
 import lombok.NonNull;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -77,7 +69,6 @@ public class TranslateProcessor {
             String targetLocaleLanguage = flagsMap.get("-targetLocaleLanguage");
             JavascriptExecutor js = (JavascriptExecutor) driver;
 
-            //js.executeScript("var imageDiv = document.getElementsByClassName('IDvEJb')[0];imageDiv.style.width = 1800 + 'px'; imageDiv.style.height = 2520 + 'px';");
             js.executeScript(
                     "var imageDiv = document.getElementsByClassName('IDvEJb')[0];"
                     + "var originalImage = document.getElementsByTagName('img')[0];"
@@ -145,118 +136,12 @@ public class TranslateProcessor {
                     + "wordsPanel.remove();");
 
             WebElement translatedImageDiv = driver.findElement(By.className("IDvEJb"));
-            getFullNodeScreenshot(driver, translatedImageDiv, outputPath, screenshotsBaseName);
+            SeleniumScreenshot.getFullNodeScreenshot(driver, translatedImageDiv, outputPath, screenshotsBaseName + "-" + String.format("%03d", ++translatedQuantity));
         } catch (Exception e) {
             System.out.println("Error processing page.");
             e.printStackTrace(System.out);
         }
         return true;
-    }
-
-    /**
-     * TODO: Definición de {@code getFullNodeScreenshot}.
-     *
-     * @param driver
-     * @param node
-     * @param outputPath
-     * @param imgBaseName
-     */
-    public static void getFullNodeScreenshot(@NonNull WebDriver driver, WebElement node, String outputPath, String imgBaseName) {
-        try {
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            File scrFile;
-            File output;
-            Point location = node.getLocation();
-            Dimension size = node.getSize();
-            System.out.println("Pos" + location);
-            System.out.println("Size" + size);
-
-            int startX = location.getX();
-            int startY = location.getY();
-            int width = 0;
-            int height = 0;
-            int posX = 0;
-            int posY = 0;
-            int i = 0;
-            int j = 0;
-
-            for (i = 0; i <= 100; i++) {
-                if (width * i >= size.getWidth())
-                    break;
-                else if ((width * (i + 1)) >= size.getWidth())
-                    posX = (startX + (size.getWidth() - width));
-                else
-                    posX = (startX + (width * i));
-
-                for (j = 0; j <= 100; j++) {
-                    if (height * j >= size.getHeight())
-                        break;
-                    else if ((height * (j + 1)) >= size.getHeight())
-                        posY = (startY + (size.getHeight() - height));
-                    else
-                        posY = (startY + (height * j));
-
-                    js.executeScript("window.scrollTo(" + posX + "," + posY + ");");
-                    //Thread.sleep(1);
-                    scrFile = ((TakesScreenshot) node).getScreenshotAs(OutputType.FILE);
-                    output = new File(outputPath + "\\" + imgBaseName + "-" + (i + 1) + "-" + (j + 1) + ".png");
-                    Files.copy(scrFile, output);
-                    if (j == 0) {
-                        BufferedImage bimg = ImageIO.read(output);
-                        width = bimg.getWidth();
-                        height = bimg.getHeight();
-                        //System.out.println("Image(" + width + ", " + height + ")");
-                    }
-
-                    //System.out.print(j + ": Start(" + posX + "," + posY + ")");
-                    //System.out.println("   End(" + (posX + width) + "," + (posY + height) + ")");
-                }
-            }
-            mergeImages(size, outputPath, imgBaseName, i, j);
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        }
-    }
-
-    /**
-     * TODO: Definición de {@code mergeImages}.
-     *
-     * @param size
-     * @param outputPath
-     * @param imgBaseName
-     * @param rows
-     * @param columns
-     */
-    public static void mergeImages(Dimension size, String outputPath, String imgBaseName, int rows, int columns) {
-        try {
-            //System.out.println("Size" + size);
-            BufferedImage outBuff = new BufferedImage(size.getWidth(), size.getHeight(), BufferedImage.TYPE_INT_ARGB);
-            Graphics graphic = outBuff.getGraphics();
-
-            int startX = 0;
-            int startY = 0;
-            BufferedImage first = ImageIO.read(new File(outputPath + "\\" + imgBaseName + "-" + (1) + "-" + (1) + ".png"));
-            for (int i = 0; i < rows; i++) {
-                if (i != rows - 1)
-                    startX = i * first.getWidth();
-                else
-                    startX = size.getWidth() - first.getWidth();
-                for (int j = 0; j < columns; j++) {
-                    File imgFile = new File(outputPath + "\\" + imgBaseName + "-" + (i + 1) + "-" + (j + 1) + ".png");
-                    BufferedImage img = ImageIO.read(imgFile);
-                    imgFile.delete();
-                    if (j != columns - 1)
-                        startY = j * img.getHeight();
-                    else
-                        startY = size.getHeight() - img.getHeight();
-                    graphic.drawImage(img, startX, startY, null);
-                }
-            }
-            ImageIO.write(outBuff, "PNG", new File(outputPath + "\\" + imgBaseName + "-" + String.format("%03d", ++translatedQuantity) + ".png"));
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        }
-
     }
 
     /**
@@ -332,7 +217,6 @@ public class TranslateProcessor {
 
                 System.out.println("Finish processing pages...");
                 driver.quit();
-                //mergeImages(new Dimension(1800, 2520), outputPath, screenshotsBaseName, 2, 5);
             }
         }
         return result;
